7384b396ce57d41248bc18c485c893dddc851d77
chefkasperson <chefkasperson@gmail.com>
2022-03-07 14:06:18 -0800

    template of layout

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-    </View>
+ <>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{padding: 16, backgroundColor: 'green'}}>
+        <Text>Search</Text>
+      </View>
+      <View style={{flex: 1, padding: 16, backgroundColor: 'blue'}}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
+    <StatusBar style='auto' />
+ </>
   );
 }
 
