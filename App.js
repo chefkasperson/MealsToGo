1f6a65f2d454b97de03b1e08b48ac9a6116309e9
chefkasperson <chefkasperson@gmail.com>
2022-03-09 02:25:59 -0800

    added theme for styled components

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, StatusBar } from "react-native";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { Searchbar } from "react-native-paper";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme/index";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <StatusBar style="auto" />
     </>
   );
